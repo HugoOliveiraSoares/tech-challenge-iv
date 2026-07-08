13de084fb18589bdb2394ab69bb073a285395ce9
Lucas Oliveira <luc.osilva@gmail.com>
2026-07-08 02:21:16 -0300

    add feedback api error handling tests

--- apps/feedback-api/src/test/java/br/com/fiap/feedbackapi/infra/http/AvaliacaoResourceTest.java
+++ apps/feedback-api/src/test/java/br/com/fiap/feedbackapi/infra/http/AvaliacaoResourceTest.java
@@ -23,4 +23,125 @@ class AvaliacaoResourceTest {
                 .body("urgencia", equalTo("CRITICA"))
                 .body("dataEnvio", notNullValue());
     }
+
+    @Test
+    void deveRetornar400QuandoJsonMalformado(){
+        given()
+                .contentType("application/json")
+                .body("""
+                  {
+                    "descricao": "Testando Json mal formado",
+                    "nota":
+                  }
+                  """)
+                .when().post("/avaliacao")
+                .then()
+                .statusCode(400)
+                .header(HttpHeadersName.X_CORRELATION_ID, notNullValue())
+                .body("code", equalTo("MALFORMED_JSON"))
+                .body("message", equalTo("Corpo da requisição nao é um JSON válido"))
+                .body("correlationId", notNullValue())
+                .body("details", notNullValue());
+    }
+
+    @Test
+    void deveRetornar400QuandoDescricaoAusente(){
+        given()
+                .contentType("application/json")
+                .body("""
+                  {
+                    "nota": 8
+                  }
+                  """)
+                .when().post("/avaliacao")
+                .then()
+                .statusCode(400)
+                .header(HttpHeadersName.X_CORRELATION_ID, notNullValue())
+                .body("code", equalTo("VALIDATION_ERROR"))
+                .body("message", equalTo("Campos obrigatórios ausentes ou inválidos"))
+                .body("correlationId", notNullValue())
+                .body("details", notNullValue());
+    }
+
+    @Test
+    void deveRetornar400QuandoNotaAusente(){
+        given()
+                .contentType("application/json")
+                .body("""
+                  {
+                    "descricao": "Testando nota ausente"
+                  }
+                  """)
+                .when().post("/avaliacao")
+                .then()
+                .statusCode(400)
+                .header(HttpHeadersName.X_CORRELATION_ID, notNullValue())
+                .body("code", equalTo("VALIDATION_ERROR"))
+                .body("message", equalTo("Campos obrigatórios ausentes ou inválidos"))
+                .body("correlationId", notNullValue())
+                .body("details", notNullValue());
+    }
+
+    @Test
+    void deveRetornar422QuandoDescricaoCurta(){
+        given()
+                .contentType("application/json")
+                .body("""
+                  {
+                    "descricao": "Hi",
+                    "nota": 9
+                  }
+                  """)
+                .when().post("/avaliacao")
+                .then()
+                .statusCode(422)
+                .header(HttpHeadersName.X_CORRELATION_ID, notNullValue())
+                .body("code", equalTo("BUSINESS_RULE_ERROR"))
+                .body("message", equalTo("Regra de negocio violada"))
+                .body("correlationId", notNullValue())
+                .body("details[0].field", equalTo("descricao"))
+                .body("details[0].message", equalTo("size must be between 10 and 1000"));
+    }
+
+    @Test
+    void deveRetornar422QuandoNotaMenorQueZero(){
+        given()
+                .contentType("application/json")
+                .body("""
+                  {
+                    "descricao": "Testando nota menor que zero",
+                    "nota": -1
+                  }
+                  """)
+                .when().post("/avaliacao")
+                .then()
+                .statusCode(422)
+                .header(HttpHeadersName.X_CORRELATION_ID, notNullValue())
+                .body("code", equalTo("BUSINESS_RULE_ERROR"))
+                .body("message", equalTo("Regra de negocio violada"))
+                .body("correlationId", notNullValue())
+                .body("details[0].field", equalTo("nota"))
+                .body("details[0].message", equalTo("must be greater than or equal to 0"));
+    }
+
+    @Test
+    void deveRetornar422QuandoNotaMaiorQueDez(){
+        given()
+                .contentType("application/json")
+                .body("""
+                  {
+                    "descricao": "Testando nota maior que dez",
+                    "nota": 11
+                  }
+                  """)
+                .when().post("/avaliacao")
+                .then()
+                .statusCode(422)
+                .header(HttpHeadersName.X_CORRELATION_ID, notNullValue())
+                .body("code", equalTo("BUSINESS_RULE_ERROR"))
+                .body("message", equalTo("Regra de negocio violada"))
+                .body("correlationId", notNullValue())
+                .body("details[0].field", equalTo("nota"))
+                .body("details[0].message", equalTo("must be less than or equal to 10"));
+    }
 }
